b08e1cd32a0c9d24db872cf7e12c201e9064bdcf
BrundaRachuaiah <brundadr315@gmail.com>
2026-01-14 11:28:08 +0530

    shopify routes added

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App.jsx'
+import { initShopifyAppBridge } from "./shopifyAppBridge";
 
 // ✅ INIT SHOPIFY APP BRIDGE FIRST
 initShopifyAppBridge();
